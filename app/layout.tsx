dbdfaaf2b32e0b9f995e7f23e8c31234033d6a3f
andika <zoneandika@gmail.com>
2025-05-31 16:59:49 +0700

    font dan analitik

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,28 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { Navbar } from "@/components/navbar"
-import { Footer } from "@/components/footer"
-
-const inter = Inter({ subsets: ["latin"] })
+import type React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
+import { Navbar } from "@/components/navbar";
+import { Footer } from "@/components/footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
-  title: "Tech Blog",
+  title: "Recoyu",
   description: "A simple blog about technology, reviews, and buying guides",
-    generator: 'v0.dev'
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#0a1929] text-gray-100`}>
+      <body className={` bg-[#0a1929] text-gray-100`}>
+        <Analytics />
         <Navbar />
         <main className="min-h-screen">{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
